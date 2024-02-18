36719b7c4f03e8c7768f605884e22982b921eb44
Noa Erben <noa.erben@post.runi.ac.il>
2024-02-19 00:02:32 +0200

    Added form to my website

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 	4) logic.js
 	5) no-transition-on-resize.js
 	
+**Q2** Query is a fast, small, and feature-rich JavaScript library. It makes things like HTML document traversal and manipulation, event handling, animation, and Ajax much simpler with an easy-to-use API that works across a multitude of browsers.
---
+++ myWeb/index.js
@@ -0,0 +1,41 @@
+let selectedWord;
+let shuffleWord;
+
+function paint(color) {
+    const circle = document.getElementById('circleID');
+    circle.style = `background-color:${color}`;
+}
+
+function startGame() {
+    document.getElementById("fguess").value = "";
+    const wordList = ["hello", "html", "css", "noa" , "computer" , "university", "friends"];
+    const randomIndex = Math.floor(Math.random() * wordList.length );
+    selectedWord = wordList[randomIndex];
+    shuffleWord = selectedWord.shuffle();
+
+    document.getElementById("gameTextID").innerHTML = shuffleWord;
+}
+
+String.prototype.shuffle = function () {
+    var a = this.split(""),
+        n = a.length;
+
+    for(var i = n - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+    return a.join("");
+}
+
+function check(){
+    const guess = document.getElementById("fguess").value;
+    if (guess === selectedWord){
+        document.getElementById("gameResultID").innerHTML = "success!";
+    }
+    else{
+        document.getElementById("gameResultID").innerHTML = "try again!";
+    }
+}
+
